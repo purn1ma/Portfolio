e7186e5c58b386af80ccd357861e38951878dadc
Purnima Shrivastava <purnimashrivastava97@gmail.com>
2023-12-03 16:45:57 +0530

    Add temp Logo + Add Layout + Add Sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import useWindowDimensions from '../../hooks/useWindowDimension'
+import Sidebar from '../Sidebar'
+import BottomBar from '../BottomBar'
+import { Outlet } from 'react-router-dom'
 
-function index() {
+import './index.scss'
+
+const Layout = () => {
+  const { width } = useWindowDimensions()
   return (
-    <div>index</div>
+    <div className="app">
+      <div className="navbar">
+        {width > 480 && <Sidebar />}
+        {width <= 480 && <BottomBar />}
+      </div>
+      <div className="page">
+        <span className="tags top-tags">
+          <span className="top-tag-doctype"> &lt;!doctype portfolio&gt;</span>
+          <br />
+          <span className="top-tag-html"> &lt;html&gt;</span>
+          <br />
+          <span className="body-tag"> &lt;body&gt;</span>
+        </span>
+
+        <Outlet />
+
+        <span className="tags bottom-tags">
+          <span className="body-tag"> &lt;/body&gt;</span>
+          <br />
+          <span className="bottom-tag-html"> &lt;/html&gt;</span>
+          <br />
+          <span className="bottom-thanks"> Thanks for visiting my page!</span>
+        </span>
+      </div>
+    </div>
   )
 }
 
-export default index
+export default Layout
