c647063d3dfb4e937d8eca35ad5a88b707d11908
Purnima Shrivastava <purnimashrivastava97@gmail.com>
2023-12-03 12:43:16 +0530

    Add Basic Layout of project + Add Fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.scss'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
